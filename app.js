a5c5f8cde91bf4fb88f65ded6f689a4e401012d9
satrajyadav196 <santrajyadav196@gmail.com>
2021-07-13 23:00:56 +0530

    last One

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const MongoStore = require('connect-mongo');
 
 // const dbUrl = process.env.DB_URL;
 
-const dbUrl = process.eventNames.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
